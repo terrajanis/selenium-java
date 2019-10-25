05ac418d976d164199709e8c206ab31da665af22
Anna Yargina <terrajanis@yandex.ru>
2019-10-25 17:57:19 +0300

    Homework 8 ver 2

--- litecart/src/test/java/ru/stqa/training/selenium/CheckStickersTest.java
+++ litecart/src/test/java/ru/stqa/training/selenium/CheckStickersTest.java
@@ -13,7 +13,7 @@ public class CheckStickersTest extends TestBase {
     @Test
     public void checkStickers() {
         driver.get("http://localhost/litecart/en/");
-       List<WebElement> ducks = driver.findElements(By.cssSelector("li.product.column.shadow.hover-light"));
+       List<WebElement> ducks = driver.findElements(By.cssSelector("li.product"));
        for (WebElement duck : ducks) {
            List<WebElement> sticker = duck.findElements(By.cssSelector("div.sticker"));
            assertEquals(1, sticker.size());
